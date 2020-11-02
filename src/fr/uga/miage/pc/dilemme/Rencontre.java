7bc6adf1e9827bccea850ca439337df43844b3c9
AurelienAVZN <cita.avan@gmail.com>
2020-11-02 15:02:02 +0100

    Rencontre

--- src/fr/uga/miage/pc/dilemme/Rencontre.java
+++ src/fr/uga/miage/pc/dilemme/Rencontre.java
@@ -8,13 +8,14 @@ import java.util.*;
  */
 
 public class Rencontre {
-	Strategie s1;
-	Strategie s2;
-	ArrayList<Tour> rounds;
+	private Strategie s1;
+	private Strategie s2;
+	private int[] scoresFinal;
 	
 	public Rencontre(Strategie s1, Strategie s2) {
 		this.s1 = s1;
 		this.s2 = s2;
+		this.scoresFinal = new int[] {0, 0};
 	}
 	
 	public Strategie getStrategie1() {
@@ -33,11 +34,50 @@ public class Rencontre {
 		this.s2 = s2;
 	}
 	
+	public int[] getFinalScores() {
+		return this.scoresFinal;
+	}
+	
+	public int getFinalScoreS1() {
+		return this.scoresFinal[0];
+	}
+	
+	public int getFinalScoreS2() {
+		return this.scoresFinal[1];
+	}
+	
 	public String toString() {
 		return "La rencontre oppose " + this.s1.getNomStrategie() + " et " + this.s2.getNomStrategie();
 	}
 	
-	public void lauch() {
-		
+	public void lauch(int nbTours) {
+		this.s1.clear();
+		this.s2.clear();
+		for(int i = 0; i < nbTours; i++) {
+			String ps1 = this.s1.play();
+			String ps2 = this.s2.play();
+			this.s1.setPlay(ps2);
+			this.s2.setPlay(ps1);
+			this.scoreTour(ps1, ps2);
+		}
+	}
+	
+	private void scoreTour(String ps1, String ps2) {
+		if(ps1.equals("c") && ps2.equals("c")) {
+			this.scoresFinal[0] += 3;
+			this.scoresFinal[1] += 3;
+		} else if (ps1.equals("t") && ps2.equals("t")) {
+			this.scoresFinal[0]++;
+			this.scoresFinal[1]++;
+		} else if (ps1.equals("n") || ps2.equals("n")) {
+			this.scoresFinal[0] += 2;
+			this.scoresFinal[1] += 2;
+		} else if ((ps1.equals("t") && ps2.equals("c")) || (ps1.equals("c") && ps2.equals("t"))) {
+			if(ps1.equals("t")) {
+				this.scoresFinal[0] += 5;
+			} else {
+				this.scoresFinal[1] += 5;
+			}
+		}
 	}
 }
--- test/fr/uga/miage/pc/dilemme/TestRencontre.java
+++ test/fr/uga/miage/pc/dilemme/TestRencontre.java
@@ -1,14 +1,66 @@
 package fr.uga.miage.pc.dilemme;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import fr.uga.miage.pc.dilemme.strategie.Gentille;
+import fr.uga.miage.pc.dilemme.strategie.Mechante;
 
-class TestRencontre {
+class TestRencontre extends Rencontre{
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	public TestRencontre() {
+		super(new Gentille(), new Mechante());
 	}
 
+	@Test
+	void testGetStrategie1() {
+		Strategie s = this.getStrategie1();
+		assertTrue(s.equals(new Gentille()));
+	}
+	
+	@Test
+	void testGetStrategie2(){
+		Strategie s = this.getStrategie2();
+		assertTrue(s.equals(new Mechante()));
+	}
+	
+	@Test
+	void testSetStrategie1() {
+		
+	}
+	
+	@Test
+	void testSetStrategie2() {
+		
+	}
+	
+	@Test
+	void testGetFinalScores() {
+		this.lauch(20);
+		assertEquals(0, this.getFinalScores()[0]);
+		assertEquals(100, this.getFinalScores()[1]);
+	}
+	
+	@Test
+	void testGetFinalScoreS1() {
+		this.lauch(20);
+		assertEquals(0, this.getFinalScoreS1());
+	}
+	
+	@Test
+	void testGetFinalScoreS2() {
+		this.lauch(20);
+		assertEquals(100, this.getFinalScoreS2());
+	}
+	
+	@Test
+	void testToString() {
+		assertEquals("La rencontre oppose " + this.getStrategie1().getNomStrategie() + " et " + this.getStrategie2().getNomStrategie(), this.toString());
+	}
+	
+	@Test
+	void testLaunch() {
+		this.lauch(20);
+		assertEquals(0, this.getFinalScoreS1());
+		assertEquals(100, this.getFinalScoreS2());
+	}
 }
